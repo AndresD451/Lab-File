bb364c5d50d1a566ccf34e0541df9f0102f9e428
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-02-27 08:25:09 -0600

    Clase GUI y comandos

--- CMD/src/cmd/CMD.java
+++ CMD/src/cmd/CMD.java
@@ -14,7 +14,8 @@ public class CMD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GUI ventana = new GUI();
+        
     }
     
 }
---
+++ CMD/src/cmd/GUI.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cmd;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author user
+ */
+public class GUI extends JFrame {
+    
+    public GUI(){
+        setTitle("Mi Ventana");
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setVisible(true);
+    }
+    
+}
